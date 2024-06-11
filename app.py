057e4a8f4a9735c9a712506f66b64d9f3f72297f
anjaanii <amohil_be21@thapar.edu>
2024-06-12 04:49:34 +0530

    yeses

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
+@app.route('/quality')
+def quality():
+    return render_template('quality.html')
 
 @app.route('/pricing')
 def pricing():
