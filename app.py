1857262d7cec2c1c8de00b299516bad5274be035
Chandankawatra123 <ckawatra_be21@thapar.edu>
2024-06-11 17:01:42 +0530

    ss

--- app.py
+++ app.py
@@ -2,9 +2,24 @@ from flask import Flask, render_template
 app=Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-    # return 'Hello, World!'
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+
+    
 
 if __name__=="__main__":
     app.run(debug=True)
